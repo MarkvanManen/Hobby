dc74cff06aaadaedcfaca2855ae6e44d608dae63
Mark <mark.vanmanen@student.hu.nl>
2017-11-10 13:46:02 +0100

    commentaar

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 var coins = 0;
 var cursors = 0;
-var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));
+//var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));
 var nextCost = Math.floor(10 * Math.pow(1.1,cursors));
 var cursorUpgrades = 0;
-
+//var cursorUpgradeCost = Math.floor(100 * Math.pow(2.5,cursorUpgrades));
 var nextUpgradeCost = Math.floor(100 * Math.pow(2.5,cursorUpgrades));
 var clickUpgrades = 0;
 var clickUpgradeCost = Math.floor(150 * Math.pow(1.5,clickUpgrades));
 var timer = Math.round(1000 - 100 * cursorUpgrades)
 var coinsPerSecond = round((cursors * 1000 / timer), 2);
 
+//Elke muisklik op de knop geeft 1 coins, met de click upgrade wordt dit aantal verdubbeld.
 function coinClick(number){
 	if(clickUpgrades >= 1){
 	coins = coins + (number * (clickUpgrades * 2));
@@ -19,20 +20,23 @@ function coinClick(number){
 	document.getElementById("coins").innerHTML = coins;
 };
 
+//Elke cursor geeft 1 coin per seconde (1000 millieseconde), elke cursor upgrade haalt hier 100 millieseconde vanaf waardoor de cursors dus 'sneller' werken.
 function periodicall() {
     coinClick(cursors);
-    //if you change cursorUpgrades, your timer will change
     timer = Math.round(1000 - 100 * cursorUpgrades)
     console.log(timer);
     setTimeout(periodicall, timer);
 };
 periodicall();
 
+//kleine functie om getallen netjes af te ronden, verder niet belangrijk.
 function round(value, precision) {
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
 }
 
+//Er wordt gekeken of het aantal coins dat je hebt groter dan of gelijk aan de prijs van een cursor is. Is dit het geval dan update je aantal coins, 
+//aantal cursors en het aantal Coins per seconde.
 function buyCursor(){
 	var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));
 	if(coins >= cursorCost){
@@ -41,6 +45,7 @@ function buyCursor(){
 		coinsPerSecond = round((cursors * 1000 / timer), 2);
 		document.getElementById('cursors').innerHTML = cursors;
 		document.getElementById('coins').innerHTML = coins;
+		//hier wordt coinsPerSecond dus in HTML ge-update, en dit zie je direct terug. Later in de code doe ik exact hetzelfde maar werkt het niet.
 		document.getElementById('coinsPerSecond').innerHTML = coinsPerSecond;
 		console.log("1 cursor bought.")
 	} else {
@@ -50,18 +55,29 @@ function buyCursor(){
 	document.getElementById('cursorCost').innerHTML = nextCost;
 };
 
+//Hier zit mijn probleem!
+//Het werkt hetzelfde als bij de functie voor het kopen van een cursor, en deze functie werkt verder ook goed, behalve het aantal coinsPerSecond.
 function buyCursorUpgrade(){
-	if(coins >= Math.floor(100 * Math.pow(2.5,cursorUpgrades)) && cursorUpgrades < 9){
-		var cursorUpgradeCost = Math.floor(100 * Math.pow(2.5,cursorUpgrades));
+	var cursorUpgradeCost = Math.floor(100 * Math.pow(2.5,cursorUpgrades));
+	//9 is het maximale aantal upgrades dat er gekocht mag worden.
+	if(coins >= cursorUpgradeCost && cursorUpgrades < 9){
 		cursorUpgrades = cursorUpgrades + 1;
-		coins = coins - cursorUpgradeCost;
+		coins = coins - Math.floor(100 * Math.pow(2.5,cursorUpgrades));
+		//Hier doe ik dus hetzelfde als op regel 45 en 49, alleen werkt het daar wel maar hier niet.
 		coinsPerSecond = round((cursors * 1000 / timer), 2);
-		updatecps();
+		document.getElementById('coinsPerSecond').innerHTML = coinsPerSecond;
 		document.getElementById('cursorUpgrades').innerHTML = cursorUpgrades;
 		document.getElementById('coins').innerHTML = coins;
-		console.log(cursors * 1000 / timer)
-		document.getElementById('coinsPerSecond').innerHTML = coinsPerSecond;
-		console.log("cursor upgrade bought.")
+		//Ik print de formule van CPS en ook de variabele zelf, maar beiden geven nooit het juiste getal.
+		//Wat bedoel ik met het juiste getal:
+		//Stel ik koop 5 cursors, dan krijg ik elke seconde 5 coins, oftewel coinsPerSecond = 5. Dit werkt allemaal prima.
+		//Vervolgens koop ik een cursorUpgrade, nu krijg ik dus 5 coins per 900 millieseconde, omgerekend is dit 5.56 coinsPerSecond.
+		//Na het kopen van deze upgrade krijg ik ook daadwerkelijk 5 coins per 900 millieseconde, ik zie dat het sneller gaat hoe meer upgrades ik koop.
+		//Echter verandert het aantal coinsPerSecond niet, niet op het scherm en niet in de console. Het update pas als ik een nieuwe cursor koop of als ik de 
+		//functie updatecps() gebruik. Dit is vreemd want updatecps() bevat exact dezelfde code die ik hier uitvoer, en hier verwijs ik zelf nog naar updatecps()
+		console.log (round((cursors * 1000 / timer), 2);
+		console.log(coinsPerSecond);
+		console.log("cursor upgrade bought.");
 	} else if(cursorUpgrades == 9){
 		document.getElementById('cursorUpgradeCost').innerHTML = "Maximum";
 		console.log('maximum cursor upgrades')
@@ -71,10 +87,14 @@ function buyCursorUpgrade(){
 	var nextUpgradeCost = Math.floor(100 * Math.pow(2.5,cursorUpgrades));
 	document.getElementById('cursorUpgradeCost').innerHTML = nextUpgradeCost;
 	console.log("new cost updated.")
+	//Hier print ik het nog een keer maar ook hier geeft het de verkeerde value.
 	console.log(cursors * 1000 / timer)
+	//Deze functie kan ook naar verwijzen worden met een knop op de html pagina, als ik de knop gebruik laat hij wel de goede waarde zien, maar als ik
+	//hier naar dezelfde functie verwijs al de knop werkt het niet en blijft hij de verkeerde value geven.
 	updatecps();
 };
 
+//Spreekt voor zich, irrelevant.
 function buyClickUpgrade(){
 	if(coins >= Math.floor(150 * Math.pow(1.5,clickUpgrades))){
 		coins = coins - Math.floor(150 * Math.pow(1.5,clickUpgrades));
@@ -88,6 +108,7 @@ function buyClickUpgrade(){
 	document.getElementById('clickUpgradeCost').innerHTML = nextClickUpgradeCost;
 };
 
+//irrelevant
 function save(){
 	localStorage.setItem("coins",JSON.stringify(coins));
 	localStorage.setItem("cursors",JSON.stringify(cursors));
@@ -96,6 +117,7 @@ function save(){
 	console.log('Saved.');
 };
 
+//irrelevant
 function load(){
 	coins = JSON.parse(localStorage.getItem('coins'));
 	cursors = JSON.parse(localStorage.getItem('cursors'));
@@ -118,6 +140,7 @@ function load(){
 	console.log('Loaded.');
 };
 
+//irrelevant
 function resetgame(){
 	coins = 0;
 	cursors = 0;
@@ -135,23 +158,13 @@ function resetgame(){
 	document.getElementById('clickUpgradeCost').innerHTML = clickUpgradeCost;
 };
 
+//Dit is de functie waar ik het eerder over had. Als ik in de html pagina op de knop "Buy cursor upgrade" klik werkt alles behalve het aantal CPS dat niet aanpast.
+//Als ik op de knop "update CPS" druk dan laat hij opeens wel de juiste CPS waarde zien, terwijl de code in de functie hieronder exact hetzelfde bevat als in
+//de functie buyCursorUpgrade, sterker nog in buyCursorUpgrade verwijs ik zelf naar de functie updatecps en alsnog werkt het niet.
 function updatecps(){
 	console.log(round((cursors * 1000 / timer), 2))
 	console.log(coinsPerSecond)
 	document.getElementById('coinsPerSecond').innerHTML = round((cursors * 1000 / timer), 2);
 }
 
-/*
-if (coins == 0 && cursors == 0) {
-		coins = savegamecoins;
-		cursors = savegamecursors;
-		cursorCost = Math.floor(10 * Math.pow(1.1,cursors));
-		document.getElementById('cursors').innerHTML = cursors;
-		document.getElementById('coins').innerHTML = coins;
-		document.getElementById('cursorCost').innerHTML = cursorCost;
-};
-*/
-
-//cursorUpgradeCost en formule niet gelijk
-//CPS wordt niet in function aangepast, pas later.
 
